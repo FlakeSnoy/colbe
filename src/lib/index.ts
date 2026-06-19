f9e03ee29efaac656cf4338e1e6b5d3b7fcbd0eb
FlameFragsC <Malebochester@proton.me>
2026-06-19 19:17:11 +0200

    feat: patch attempt #2

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export { default as Navbar } from './components/marketing/navbar.svelte';
 export { default as Hero } from './components/marketing/hero.svelte';
-export { default as Features } from '.components/marketing/features.svelte';
+export { default as Features } from './components/marketing/features.svelte';
 export { default as Cta } from './components/marketing/cta.svelte';
 export { default as Footer } from './components/marketing/footer.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,14 @@
 	import { Navbar, Hero, Features, Cta, Footer } from '$lib/index.js';
 </script>
 
+<svelte:head>
+	<title>Colbe</title>
+	<meta
+		name="description"
+		content="Colbe - Platform"
+	/>
+</svelte:head>
+
 <Navbar />
 <Hero />
 <Features />
